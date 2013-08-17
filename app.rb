f86cc1ad2f0d8c053cdea705f1d2bc1da2b7086f
Rob Miller <rob@bigfish.co.uk>
2013-08-17 21:15:08 +0100

    Display last fetched date below real table

--- app.rb
+++ app.rb
@@ -3,13 +3,13 @@ Bundler.setup
 
 require 'sinatra'
 require 'haml'
-
 require 'active_support'
 
 require 'data_mapper'
 DataMapper::Logger.new($stdout, :debug)
 DataMapper.setup(:default, "sqlite://#{File.dirname(__FILE__)}/predictions.db")
 
+require_relative 'helpers/init'
 require_relative 'lib/models'
 
 require 'letters'
---
+++ helpers/date.rb
@@ -0,0 +1,5 @@
+class DateTime
+  def to_human
+    strftime("%e %B, %H:%M")
+  end
+end
