af4a6411612dec3edbb21f0563058b333be45923
Rob Miller <rob@bigfish.co.uk>
2013-08-18 16:21:51 +0100

    Refactor the BBC library

--- lib/bbc.rb
+++ lib/bbc.rb
@@ -7,19 +7,53 @@ class BBC
   base_uri 'www.bbc.co.uk/sport/football/'
 
   def table
+    return get_table_cache unless table_needs_updating?
+
+    html = fetch_table
+    positions = extract_positions(html)
+
+    return get_table_cache unless positions.length > 0
+
+    clear_table_cache
+
+    update_table_cache(positions)
+  end
+
+  # Checks whether the league table needs to be fetched from the BBC
+  # website, or whether it's been updated recently enough that we can
+  # use the cached copy.
+  def table_needs_updating?
     first = RealPosition.first
     last_fetched = first ? first.fetched : DateTime.new(0)
+    last_fetched.since(3600).past?
+  end
 
-    # If we've fetched the real league table recently enough, then
-    # return the one from the database.
-    return RealPosition.all if last_fetched.since(3600).future?
-
+  # Clears any stored league table that we have
+  def clear_table_cache
     RealPosition.all.destroy
+  end
 
-    doc = Nokogiri::HTML(self.class.get('/premier-league/table'))
-    positions = doc.css('.league-table tbody[data-full-entry-count="20"] td.team-name a')
+  # Fetches positions from the cache
+  def get_table_cache
+    RealPosition.all
+  end
+
+  # Returns the HTML of the league table page on the BBC website, from
+  # which we can extract the teams in order of position.
+  def fetch_table
+    self.class.get('/premier-league/table')
+  end
+
+  # Parses the markup of the page and extracts the teams in order of
+  # position.
+  def extract_positions(html)
+    doc = Nokogiri::HTML(html)
+    doc.css('.league-table tbody[data-full-entry-count="20"] td.team-name a')
       .map { |team| team.content }
 
+  end
+
+  def update_table_cache(positions)
     positions.each_with_index do |team, position|
       RealPosition.create(
         :team => team,
